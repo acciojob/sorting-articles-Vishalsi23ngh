64457bfdfcf1493f5a02e05b5b5c6e2ba66f2292
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-12 22:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const item = document.getElementById("articles");
+
+item.addEventListender(function () {
+
+	const inuputEle = item.value;
+	const output = inuputEle.sort();
+	item.value = output;
+	
+})
